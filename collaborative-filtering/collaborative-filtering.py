7170ef3b5b74bbe02d9afc814dea895f29b9c9da
hcvazquez <hernan.c.vazquez@gmail.com>
2019-06-06 21:29:41 -0300

    refactoring

--- collaborative-filtering/collaborative-filtering.py
+++ collaborative-filtering/collaborative-filtering.py
@@ -1,22 +1,3 @@
-# validate the shape of the data 
-print((df.count(), len(df.columns)))
-
-# check columns in dataframe
-df.printSchema()
-
-# validate few rows of dataframe in random order
-df.orderBy(rand()).show(10, False)
-
-# check number of ratings by each user
-df.groupBy('userId').count().orderBy('count', ascending=False).show(10, False)
-
-# check number of ratings by each user
-df.groupBy('userId').count().orderBy('count', ascending=True).show(10, False)
-
-# number of times movie been rated 
-df.groupBy('title').count().orderBy('count', ascending=False).show(10, False)
-df.groupBy('title').count().orderBy('count', ascending=True).show(10, False)
-
 # import String indexer to convert string values to numeric values
 from pyspark.ml.feature import StringIndexer, IndexToString
 
