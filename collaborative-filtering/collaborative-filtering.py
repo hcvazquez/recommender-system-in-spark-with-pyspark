904120afdffc39eee7c9f4d59d18a8a609655a34
hcvazquez <hernan.c.vazquez@gmail.com>
2019-06-06 21:32:54 -0300

    refactoring

--- collaborative-filtering/collaborative-filtering.py
+++ collaborative-filtering/collaborative-filtering.py
@@ -1,15 +1,3 @@
-# importing Regression Evaluator to measure RMSE
-from pyspark.ml.evaluation import RegressionEvaluator
-
-# create Regressor evaluator object for measuring accuracy
-evaluator = RegressionEvaluator(metricName='rmse', predictionCol='prediction', labelCol='rating')
-
-# apply the RE on predictions dataframe to calculate RMSE
-rmse = evaluator.evaluate(predicted_ratings)
-
-# print RMSE error
-print(rmse)
-
 # Recommend top movies  which user might like 
 
 # create dataset of all distinct movies 
